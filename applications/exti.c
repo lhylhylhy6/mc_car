876ee066a3f2fc97c4af7918f11515227f3ac008
Thesky0418 <1009141896@qq.com>
2022-09-23 18:23:48 +0800

    over

--- applications/exti.c
+++ applications/exti.c
@@ -22,154 +22,220 @@ rt_thread_t  thing_scan_thread = RT_NULL;
 
 ALIGN(RT_ALIGN_SIZE)
 
-extern int path[5];
-int a = 0;
+extern int path[8][4];
+int a = 0,status = 0;
 int thing_flag=0;
 
 #define EXT_PIN  GET_PIN(C, 8)
 #define STA_PIN  GET_PIN(C, 7)
-
-void overthemap(void)
+extern int path_num;
+extern int path_switch;
+void overthemap(void) //0022 1100
 {
+    path_num = 0;
     int temp[4]={0};
     for(int i=0;i<4;i++)
     {
-        if(path[i]==1)
+        if(path[path_switch][i]==1)
         {
-            path[i]=2;
+            path[path_switch][i]=2;
         }
-        else if(path[i]==2)
+        else if(path[path_switch][i]==2)
         {
-            path[i]=1;
+            path[path_switch][i]=1;
         }
-        temp[i] = path[i];
+        temp[i] = path[path_switch][i];
     }
     for(int i=0;i<4;i++)
     {
-        path[i] = temp[3-i];
-        LOG_D("%d, ",path[i]);
+        path[path_switch][i] = temp[3-i];
+        rt_kprintf("%d, ",path[path_switch][i]);
     }
-    LOG_D("\r\n");
+    rt_kprintf("\r\n");
     rt_kprintf("over map ok!\r\n");
 }
 
+
+int stop_num = 0,take_num = 0,stop_flag = 0;
 void irq_callback_test(void *args)
 {
-        a = 1;
-    //car_stop();
-}
-//1：正常启动 2：转弯
-void thing_scan_entry(void *parameter)
-{
-    while(1)
-    {
+     stop_num++;
+     if(stop_num == 1)
+     {
+        car_stop();
+        overthemap();
+        stop_flag = 1;
+     }
 
-        if(arrive_flag==0)
-        {
-            rt_kprintf("---now arrive flag is %d ---\r\n",arrive_flag);
-            LOG_D("a\r\n");
-            static int nums=0;
-            for(int i=0;i<10;i++)
-            {
-                if(rt_pin_read(STA_PIN)==PIN_HIGH)
-                {
-                    nums++;
-                    if(nums>=9)
-                    {
-                        nums=0;
-                        arrive_flag=2;
-                        LOG_D("now is forward\r\n");
-                        rt_kprintf("now is forward\r\n");
-                        car_forward();
-                    }
-                }
-                else {
-                    rt_thread_delay(200);
-                }
-            }
-            nums = 0;
-        }
-        else if(arrive_flag ==2)
-        {
-            rt_kprintf("---now arrive flag is %d ---\r\n",arrive_flag);
-            LOG_D("now is running\r\n");
-            rt_kprintf("now is running\r\n");
-            rt_thread_delay(1000);
-        }
-        else if(arrive_flag ==1)
-        {
-            rt_kprintf("---now arrive flag is %d ---\r\n",arrive_flag);
-            LOG_D("c\r\n");
-            static int nums=0;
-            for(int i=0;i<10;i++)
-            {
-                if(rt_pin_read(STA_PIN)==PIN_LOW)
-                {
-                    nums++;
-                    if(nums>=9)
-                    {
-                        nums=0;
-                        LOG_D("now is turn\r\n");
-                        rt_kprintf("now is turn\r\n");
-                        arrive_flag=2;
-                        if(arrive_flag==2)
-                        {
-                           overthemap();
-                           car_turn();
-                        }
-                        a=0;
-
-                    }
-                }
-                else {
-                    rt_thread_delay(200);
-                }
-            }
-            nums = 0;
-        }
-    }
 }
-int stop_numbers = 0;
+
 void car_stop_entry(void *parameter)
 {
     while(1)
     {
-        if(a==1)
-        {
-            car_stop();
-            static int nums=0;
-            stop_numbers ++;
-            if(stop_numbers == 2)
-            {
-                arrive_flag = 0;
-                stop_numbers = 0;
-            }
-            //rt_kprintf("should stop\r\n");
-            if(rt_pin_read(EXT_PIN)==PIN_LOW)
-            {
-                nums++;
-                if(nums==7)
-                {
-                    a=0;
-                    nums=0;
-                    if(arrive_flag == 2)
-                    arrive_flag=1;
-                    LOG_D("now is stop\r\n");
-                    rt_kprintf("now is stop\r\n");
-                    car_stop();
-
-                }
-            }
-            else
-            {
-                rt_thread_delay(10 );
-            }
-        }
-        else {
-            rt_thread_delay(500);
-        }
+//        if(stop_flag == 1)
+//            overthemap();
+        rt_thread_delay(200);
     }
 }
+int nums = 0;
+void thing_scan_irq(void *args)
+{
+    take_num++;
+    if(take_num == 1)
+    {
+        status = 1;
+    }
+}
+
+void  thing_scan_entry(void *parameter)
+{
+    while(1)
+    {
+        if((status == 1)&&(rt_pin_read(STA_PIN) == PIN_LOW))
+        {
+            for(int i=0;i<10;i++)
+            {
+                if(rt_pin_read(STA_PIN) == PIN_LOW)
+                {
+                    nums++;
+                    if(nums>=9)
+                    {
+                         status = 2;
+                         nums = 0;
+                    }
+                }
+
+            }
+            nums = 0;
+        }
+        if(status == 1 && stop_flag==0 )
+        {
+            car_forward();
+        }
+        if(status == 2)
+        {
+            car_turn();
+            stop_num = 0;
+            take_num = 0;
+            status = 0;
+            stop_flag = 0;
+        }
+        rt_thread_delay(200);
+    }
+}
+////1：正常启动 2：转弯
+//void thing_scan_entry(void *parameter)
+//{
+//    while(1)
+//    {
+//
+//        if(arrive_flag==0)
+//        {
+//            rt_kprintf("---now arrive flag is %d ---\r\n",arrive_flag);
+//            LOG_D("a\r\n");
+//            static int nums=0;
+//            for(int i=0;i<10;i++)
+//            {
+//                if(rt_pin_read(STA_PIN)==PIN_HIGH)
+//                {
+//                    nums++;
+//                    if(nums>=9)
+//                    {
+//                        nums=0;
+//                        arrive_flag=2;
+//                        LOG_D("now is forward\r\n");
+//                        rt_kprintf("now is forward\r\n");
+//                        car_forward();
+//                    }
+//                }
+//                else {
+//                    rt_thread_delay(200);
+//                }
+//            }
+//            nums = 0;
+//        }
+//        else if(arrive_flag ==2)
+//        {
+//            rt_kprintf("---now arrive flag is %d ---\r\n",arrive_flag);
+//            LOG_D("now is running\r\n");
+//            rt_kprintf("now is running\r\n");
+//            rt_thread_delay(1000);
+//        }
+//        else if(arrive_flag ==1)
+//        {
+//            rt_kprintf("---now arrive flag is %d ---\r\n",arrive_flag);
+//            LOG_D("c\r\n");
+//            static int nums=0;
+//            for(int i=0;i<10;i++)
+//            {
+//                if(rt_pin_read(STA_PIN)==PIN_LOW)
+//                {
+//                    nums++;
+//                    if(nums>=9)
+//                    {
+//                        nums=0;
+//                        LOG_D("now is turn\r\n");
+//                        rt_kprintf("now is turn\r\n");
+//                        arrive_flag=2;
+//                        if(arrive_flag==2)
+//                        {
+//                           overthemap();
+//                           car_turn();
+//                        }
+//                        a=0;
+//
+//                    }
+//                }
+//                else {
+//                    rt_thread_delay(200);
+//                }
+//            }
+//            nums = 0;
+//        }
+//    }
+//}
+//int stop_numbers = 0;
+//void car_stop_entry(void *parameter)
+//{
+//    while(1)
+//    {
+//        if(a==1)
+//        {
+//            car_stop();
+//            static int nums=0;
+//            stop_numbers ++;
+//            if(stop_numbers == 2)
+//            {
+//                arrive_flag = 0;
+//                stop_numbers = 0;
+//            }
+//            //rt_kprintf("should stop\r\n");
+//            if(rt_pin_read(EXT_PIN)==PIN_LOW)
+//            {
+//                nums++;
+//                if(nums==7)
+//                {
+//                    a=0;
+//                    nums=0;
+//                    if(arrive_flag == 2)
+//                    arrive_flag=1;
+//                    LOG_D("now is stop\r\n");
+//                    rt_kprintf("now is stop\r\n");
+//                    car_stop();
+//
+//                }
+//            }
+//            else
+//            {
+//                rt_thread_delay(10 );
+//            }
+//        }
+//        else {
+//            rt_thread_delay(500);
+//        }
+//    }
+//}
 
 int extern_interrupt_init(void)
 {
@@ -181,8 +247,10 @@ int extern_interrupt_init(void)
     err = rt_pin_attach_irq(EXT_PIN, PIN_IRQ_MODE_RISING, irq_callback_test, RT_NULL);
     err = rt_pin_irq_enable(EXT_PIN, PIN_IRQ_ENABLE);
 
+    err = rt_pin_attach_irq(STA_PIN, PIN_IRQ_MODE_RISING_FALLING, thing_scan_irq, RT_NULL);
+    err = rt_pin_irq_enable(STA_PIN, PIN_IRQ_ENABLE);
     car_stop_thread = rt_thread_create("car_stop_thread", car_stop_entry, RT_NULL, 512, 0, 200);
-    thing_scan_thread = rt_thread_create("thing_scan", thing_scan_entry, RT_NULL, 512, 5, 200);
+    thing_scan_thread = rt_thread_create("thing_scan", thing_scan_entry, RT_NULL, 512, 7, 200);
 
     if(car_stop_thread != RT_NULL)
         rt_thread_startup(car_stop_thread);
